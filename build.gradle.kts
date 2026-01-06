6e5ec133bfdc5a26ef1139efca2ab76beaa09767
Nestor Cortina <nexckycort@gmail.com>
2026-01-06 13:02:12 -0500

    feat: update GitHub Actions workflow to trigger on version tag pushes and streamline version setting

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.maven.publish) apply false
 }
 
 allprojects {
@@ -16,7 +17,7 @@ subprojects {
     apply(plugin = "org.jetbrains.kotlin.jvm")
     apply(plugin = "org.jetbrains.kotlin.plugin.serialization")
     apply(plugin = "java-library")
-    apply(plugin = "maven-publish")
+    apply(plugin = "com.vanniktech.maven.publish")
 
     configure<org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension> {
         jvmToolchain(17)
@@ -25,57 +26,36 @@ subprojects {
         }
     }
 
-    configure<JavaPluginExtension> {
-        withJavadocJar()
-        withSourcesJar()
-    }
+    configure<com.vanniktech.maven.publish.MavenPublishBaseExtension> {
+        publishToMavenCentral(com.vanniktech.maven.publish.SonatypeHost.CENTRAL_PORTAL, automaticRelease = true)
+        signAllPublications()
 
-    plugins.withType<MavenPublishPlugin> {
-        configure<PublishingExtension> {
-            publications {
-                register<MavenPublication>("maven") {
-                    afterEvaluate {
-                        from(components["java"])
-                    }
+        coordinates(group.toString(), project.name, version.toString())
 
-                    pom {
-                        name.set(project.name)
-                        description.set("Bloque SDK for Kotlin/Java")
-                        url.set("https://github.com/bloque-app/sdk-kotlin")
+        pom {
+            name.set(project.name)
+            description.set("Bloque SDK for Kotlin/Java")
+            url.set("https://github.com/bloque-app/sdk-kotlin")
 
-                        licenses {
-                            license {
-                                name.set("MIT License")
-                                url.set("https://opensource.org/licenses/MIT")
-                            }
-                        }
-
-                        developers {
-                            developer {
-                                id.set("bloque")
-                                name.set("Bloque Team")
-                                email.set("dev@bloque.team")
-                            }
-                        }
-
-                        scm {
-                            connection.set("scm:git:git://github.com/bloque-app/sdk-kotlin.git")
-                            developerConnection.set("scm:git:ssh://github.com/bloque-app/sdk-kotlin.git")
-                            url.set("https://github.com/bloque-app/sdk-kotlin")
-                        }
-                    }
+            licenses {
+                license {
+                    name.set("MIT License")
+                    url.set("https://opensource.org/licenses/MIT")
                 }
             }
 
-            repositories {
-                maven {
-                    name = "OSSRH"
-                    url = uri("https://central.sonatype.com/api/v1/publisher/upload")
-                    credentials {
-                        username = System.getenv("MAVEN_USERNAME")
-                        password = System.getenv("MAVEN_PASSWORD")
-                    }
+            developers {
+                developer {
+                    id.set("bloque")
+                    name.set("Bloque Team")
+                    email.set("dev@bloque.team")
                 }
+            }
+
+            scm {
+                connection.set("scm:git:git://github.com/bloque-app/sdk-kotlin.git")
+                developerConnection.set("scm:git:ssh://github.com/bloque-app/sdk-kotlin.git")
+                url.set("https://github.com/bloque-app/sdk-kotlin")
             }
         }
     }
